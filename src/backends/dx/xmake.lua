d51f7162f411461ecd97b9929a82fb889088365e
Yuefeng Geng <maxwellgeng@outlook.com>
2024-10-26 23:59:46 +0800

    add dx_cuda_interop option

--- src/backends/dx/xmake.lua
+++ src/backends/dx/xmake.lua
@@ -12,13 +12,15 @@ if is_plat("windows") then
     add_defines("UNICODE", "_CRT_SECURE_NO_WARNINGS")
 end
 on_load(function(target)
-    local cuda_path = os.getenv("CUDA_PATH")
-    if cuda_path then
-        target:add("includedirs", path.join(cuda_path, "include/"))
-        target:add("linkdirs", path.join(cuda_path, "lib/x64/"))
-        target:add("links", "nvrtc_static", "cudart_static", "cuda")
-        target:add("defines", "LCDX_ENABLE_CUDA")
-        if is_plat("windows") then
+    if get_config("dx_cuda_interop") then
+        local cuda_path = os.getenv("CUDA_PATH")
+        if not cuda_path then
+            utils.error("CUDA_PATH not found.")
+        else
+            target:add("includedirs", path.join(cuda_path, "include/"))
+            target:add("linkdirs", path.join(cuda_path, "lib/x64/"))
+            target:add("links", "nvrtc_static", "cudart_static", "cuda")
+            target:add("defines", "LCDX_ENABLE_CUDA")
             target:add("syslinks", "Cfgmgr32", "Advapi32")
         end
     end
