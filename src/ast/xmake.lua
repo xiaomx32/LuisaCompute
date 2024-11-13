8a58399baf70c9550799baad75d4abbf35d1799d
Mike Smith <mango@live.cn>
2024-11-14 00:16:35 +0800

    ???

--- src/ast/xmake.lua
+++ src/ast/xmake.lua
@@ -5,7 +5,7 @@ _config_project({
 })
 add_deps("lc-core", "lc-vstl")
 add_headerfiles("../../include/luisa/ast/**.h")
-set_pcxxheader("pch.h")
+-- set_pcxxheader("pch.h")
 add_files("**.cpp")
 add_defines("LC_AST_EXPORT_DLL")
 target_end()
