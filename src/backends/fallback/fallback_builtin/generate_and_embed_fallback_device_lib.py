4883a374a0dacd49572150a60baa005661baf994
Mike <mango@live.cn>
2024-12-12 20:18:41 +0800

    minor

--- src/backends/fallback/fallback_builtin/generate_and_embed_fallback_device_lib.py
+++ src/backends/fallback/fallback_builtin/generate_and_embed_fallback_device_lib.py
@@ -13,11 +13,12 @@ if __name__ == "__main__":
     subprocess.run(["clang++", "-c", "-emit-llvm", "-std=c++20", "-ffast-math", "-O3",
                     "-S", src_path, "-o", dst_path, "-fomit-frame-pointer",
                     "-fno-stack-protector", "-fno-rtti", "-fno-exceptions"])
-    with open(dst_path, "r") as f:
+    with open(dst_path, "r", newline="\n") as f:
         content = "".join(line for line in f.readlines()
                           if not line.strip().startswith("@llvm.used") and
                           not line.strip().startswith("; ModuleID") and
                           not line.strip().startswith("source_filename"))
+        content = content.replace("\n\n\n", "\n\n")
     content = content.replace("define hidden", "define private")
     # find all @luisa_fallback_wrapper_(\w+) and collect in a list
     impl_prefix = "@luisa_fallback_"
@@ -36,15 +37,19 @@ if __name__ == "__main__":
             if f"luisa.{dotted_name}.impl(" in content:
                 f.write(f"map_symbol(\"luisa.{dotted_name}.impl\", &api::luisa_fallback_{function_name});\n")
     # compile to bitcode
-    bc_path = dst_path.replace(".ll", ".bc")
-    subprocess.run(["llvm-as", dst_path, "-o", bc_path])
-    with open(bc_path, "rb") as f:
-        content = f.read()
-    print(content)
-    with open(os.path.join(builtin_dir, f"{file_name}.{system_name}.{machine_name}.inl"), "w") as f:
+    try:
+        bc_path = dst_path.replace(".ll", ".bc")
+        subprocess.run(["llvm-as", dst_path, "-o", bc_path])
+        with open(bc_path, "rb") as f:
+            content = f.read()
+    except:
+        print("Failed to compile to bitcode.")
+        content = content.encode("utf-8")
+    with open(os.path.join(builtin_dir, f"{file_name}.{system_name}.{machine_name}.inl"), "wb") as f:
         data = [f"0x{c:02x}" for c in content]
         size = len(data)
-        f.write(f'\nstatic const unsigned char luisa_fallback_backend_device_builtin_module[{size}] = {{\n')
+        f.write(
+            f'\nstatic const unsigned char luisa_fallback_backend_device_builtin_module[{size}] = {{\n'.encode("utf-8"))
         wrapped = ["    " + ", ".join(data[i: i + 16]) for i in range(0, len(data), 16)]
-        f.write(",\n".join(wrapped))
-        f.write("\n};\n")
+        f.write(",\n".join(wrapped).encode("utf-8"))
+        f.write("\n};\n".encode("utf-8"))
