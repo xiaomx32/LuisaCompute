e0c59c77fcde9551510341a81f8ea6cb49063e38
Mike Smith <mango@live.cn>
2024-12-14 12:13:04 +0800

    tuning

--- src/backends/fallback/fallback_shader.cpp
+++ src/backends/fallback/fallback_shader.cpp
@@ -219,8 +219,11 @@ FallbackShader::FallbackShader(const ShaderOption &option, Function kernel) noex
 #endif
     Clock clk;
     clk.tic();
-    auto MPM = PB.buildPerModuleDefaultPipeline(::llvm::OptimizationLevel::O3);
+    auto MPM = PB.buildPerModuleDefaultPipeline(::llvm::OptimizationLevel::O1);
     MPM.run(*llvm_module, MAM);
+    auto MPM2 = PB.buildPerModuleDefaultPipeline(::llvm::OptimizationLevel::O3);
+    MPM2.run(*llvm_module, MAM);
+
     LUISA_INFO("Optimized LLVM module in {} ms.", clk.toc());
     if (::llvm::verifyModule(*llvm_module, &::llvm::errs())) {
         LUISA_ERROR_WITH_LOCATION("Failed to verify module.");
