a706ca9f6ffbd3e87b00fc045b79bc580432adb0
Mike Smith <mango@live.cn>
2024-12-16 20:13:04 +0800

    replace tbb with nanothread

--- src/backends/fallback/fallback_command_queue.cpp
+++ src/backends/fallback/fallback_command_queue.cpp
@@ -1,81 +1,12 @@
+#ifdef LUISA_COMPUTE_ENABLE_TBB
+#include <tbb/parallel_for.h>
+#else
+#include <nanothread/nanothread.h>
+#endif
+
 #include "fallback_command_queue.h"
 
 namespace luisa::compute::fallback {
-
-#ifndef LUISA_COMPUTE_ENABLE_TBB
-
-class FallbackCommandQueueParallelContext {
-
-private:
-    std::mutex _dispatch_mutex;
-    std::condition_variable _dispatch_cv;
-    luisa::move_only_function<void(uint)> _kernel;
-    std::atomic_uint _fetch_count{};
-    std::atomic_uint _finish_count{};
-    uint _total{};
-    bool _should_stop{false};
-
-public:
-    void dispatch(luisa::move_only_function<void(uint)> &&k, uint n) noexcept {
-        _kernel = std::move(k);
-        _fetch_count.store(0u);
-        _finish_count.store(0u);
-        {
-            std::scoped_lock lock{_dispatch_mutex};
-            _total = n;
-        }
-        _dispatch_cv.notify_all();
-        // participate in the work
-        while (process_one()) {}
-        // wait for all work to finish
-        while (_finish_count.load() < n) {
-            using namespace std::chrono_literals;
-            std::this_thread::sleep_for(50us);
-        }
-    }
-
-    void notify_stop() noexcept {
-        {
-            std::scoped_lock lock{_dispatch_mutex};
-            _should_stop = true;
-        }
-        _dispatch_cv.notify_all();
-    }
-
-    [[nodiscard]] bool wait_for_work() noexcept {
-        std::unique_lock lock{_dispatch_mutex};
-        _dispatch_cv.wait(lock, [this] {
-            return _total != 0u || _should_stop;
-        });
-        return !_should_stop;
-    }
-
-    [[nodiscard]] bool process_one() noexcept {
-        auto fetch = _fetch_count.fetch_add(1u);
-        if (fetch >= _total) { return false; }
-        _kernel(fetch);
-        _finish_count.fetch_add(1u);
-        return true;
-    }
-};
-
-inline void FallbackCommandQueue::_ensure_parallel_workers_started() noexcept {
-    if (_parallel_context == nullptr) {
-        _parallel_context = luisa::make_unique<FallbackCommandQueueParallelContext>();
-        for (auto &w : _parallel_workers) {
-            w = std::thread{[this] { _run_parallel_worker_loop(); }};
-        }
-    }
-}
-
-void FallbackCommandQueue::_run_parallel_worker_loop() noexcept {
-    auto ctx = _parallel_context.get();
-    while (ctx->wait_for_work()) {
-        while (ctx->process_one()) {}
-    }
-}
-
-#endif
 
 inline void FallbackCommandQueue::_run_dispatch_loop() noexcept {
     // wait and fetch tasks
@@ -94,12 +25,9 @@ inline void FallbackCommandQueue::_run_dispatch_loop() noexcept {
         // count the finish of a task
         _total_finish_count.fetch_add(1u);
     }
-#ifndef LUISA_COMPUTE_ENABLE_TBB
-    if (_parallel_context) {
-        _parallel_context->notify_stop();
-        for (auto &w : _parallel_workers) { w.join(); }
+    if (_worker_pool != nullptr) {
+        pool_destroy(_worker_pool);
     }
-#endif
 }
 
 inline void FallbackCommandQueue::_wait_for_task_queue_available() const noexcept {
@@ -121,13 +49,10 @@ inline void FallbackCommandQueue::_enqueue_task_no_wait(luisa::move_only_functio
 }
 
 FallbackCommandQueue::FallbackCommandQueue(size_t in_flight_limit, size_t num_threads [[maybe_unused]]) noexcept
-    : _in_flight_limit{in_flight_limit} {
-#ifndef LUISA_COMPUTE_ENABLE_TBB
-    if (num_threads == 0u) {
-        num_threads = std::max<size_t>(std::thread::hardware_concurrency(), 1u);
+    : _in_flight_limit{in_flight_limit}, _worker_count{num_threads} {
+    if (_worker_count == 0u) {
+        _worker_count = std::thread::hardware_concurrency();
     }
-    _parallel_workers.resize(num_threads - 1u);
-#endif
     _dispatcher = std::thread{[this] { _run_dispatch_loop(); }};
 }
 
@@ -156,12 +81,14 @@ void FallbackCommandQueue::enqueue(luisa::move_only_function<void()> &&task) noe
 
 void FallbackCommandQueue::enqueue_parallel(uint n, luisa::move_only_function<void(uint)> &&task) noexcept {
     enqueue([this, n, task = std::move(task)]() mutable noexcept {
-#ifdef LUISA_COMPUTE_ENABLE_TBB
-        tbb::parallel_for(0u, n, task);
-#else
-        _ensure_parallel_workers_started();
-        _parallel_context->dispatch(std::move(task), n);
-#endif
+        if (_worker_pool == nullptr) {
+            _worker_pool = pool_create(_worker_count);
+        }
+        drjit::blocked_range<int> range{0, static_cast<int>(n)};
+        drjit::parallel_for(range, [&task](drjit::blocked_range<int> r) {
+            for (auto i : r) {
+                task(static_cast<uint>(i));
+            } }, _worker_pool);
     });
 }
 
--- src/backends/fallback/fallback_command_queue.h
+++ src/backends/fallback/fallback_command_queue.h
@@ -4,13 +4,10 @@
 #include <thread>
 #include <condition_variable>
 
-#ifdef LUISA_COMPUTE_ENABLE_TBB
-#include <tbb/parallel_for.h>
-#endif
+#include <nanothread/nanothread.h>
 
 #include <luisa/core/basic_types.h>
 #include <luisa/core/stl/queue.h>
-#include <luisa/core/stl/vector.h>
 #include <luisa/core/stl/functional.h>
 #include <luisa/core/stl/memory.h>
 
@@ -30,13 +27,8 @@ private:
     size_t _in_flight_limit{0u};
     std::atomic_size_t _total_enqueue_count{0u};
     std::atomic_size_t _total_finish_count{0u};
-
-#ifndef LUISA_COMPUTE_ENABLE_TBB
-    luisa::vector<std::thread> _parallel_workers;
-    luisa::unique_ptr<FallbackCommandQueueParallelContext> _parallel_context;
-    void _ensure_parallel_workers_started() noexcept;
-    void _run_parallel_worker_loop() noexcept;
-#endif
+    ::Pool *_worker_pool{nullptr};
+    size_t _worker_count{0u};
 
 private:
     void _run_dispatch_loop() noexcept;
