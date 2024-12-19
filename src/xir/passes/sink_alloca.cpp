d2ef6dbe6c202996ea9d0e5bdb83e4cea4e73b79
Mike Smith <mango@live.cn>
2024-12-19 23:21:18 +0800

    wip: sink alloca insts

--- src/xir/passes/sink_alloca.cpp
+++ src/xir/passes/sink_alloca.cpp
@@ -1,5 +1,7 @@
 #include <luisa/core/logging.h>
+#include <luisa/core/stl/unordered_map.h>
 #include <luisa/xir/function.h>
+#include <luisa/xir/instructions/alloca.h>
 #include <luisa/xir/passes/sink_alloca.h>
 
 namespace luisa::compute::xir {
@@ -7,17 +9,37 @@ namespace luisa::compute::xir {
 class AllocaInstSinker {
 
 private:
-    static void _collect_alloca_insts_in_block(luisa::vector<AllocaInst *> collected, BasicBlock *bb) noexcept {
+    static void _collect_alloca_insts_in_block(luisa::vector<AllocaInst *> &collected,
+                                               luisa::unordered_set<BasicBlock *> &visited,
+                                               BasicBlock *bb) noexcept {
+        if (visited.emplace(bb).second) {
+            for (auto &&inst : bb->instructions()) {
+                if (inst.derived_instruction_tag() == DerivedInstructionTag::ALLOCA) {
+                    collected.emplace_back(static_cast<AllocaInst *>(&inst));
+                } else {// find basic blocks
+                    for (auto &&o : inst.operand_uses()) {
+                        if (auto v = o->value(); v->derived_value_tag() == DerivedValueTag::BASIC_BLOCK) {
+                            _collect_alloca_insts_in_block(collected, visited, static_cast<BasicBlock *>(v));
+                        }
+                    }
+                }
+            }
+        }
     }
 
 private:
     [[nodiscard]] static bool _try_sink_alloca_inst(AllocaInst *inst) noexcept {
+        return false;
     }
 
 public:
     [[nodiscard]] static SinkAllocaInfo run(FunctionDefinition *f) noexcept {
-        luisa::vector<AllocaInst *> collected;
-        _collect_alloca_insts_in_block(collected, f->body_block());
+        auto collected = [f] {
+            luisa::vector<AllocaInst *> collected;
+            luisa::unordered_set<BasicBlock *> visited;
+            _collect_alloca_insts_in_block(collected, visited, f->body_block());
+            return collected;
+        }();
         collected.erase(std::remove_if(collected.begin(), collected.end(), [](AllocaInst *inst) noexcept {
                             return !_try_sink_alloca_inst(inst);
                         }),
